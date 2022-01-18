a7e31e3a7317e05bdbc253fefcfe4aead79ac0c5
Baturalp Sönmez <baturalpsonmez2002@gmail.com>
2022-01-18 23:55:43 +0300

    route'lar düzenlendi

--- crm-ui/src/App.tsx
+++ crm-ui/src/App.tsx
@@ -3,32 +3,15 @@ import "./App.css";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ASideBar from "./layouts/ASideBar";
-import menuItems from "./asidemenu";
 import HeaderBar from "./layouts/HeaderBar";
 
 function App() {
   return (
     <BrowserRouter>
-      {/* BEGIN :: ASideBar Routes */}
       <Routes>
-        {menuItems.map((item) => (
-          <Route path={item.path} element={<ASideBar />} />
-        ))}
-        <Route path="settings" element={<ASideBar />} />
-        <Route path="hr-members" element={<ASideBar />} />
-        <Route path="profile" element={<ASideBar />} />
+        <Route path="*" element={<ASideBar />} />
+        <Route path="*" element={<HeaderBar />} />
       </Routes>
-      {/* END :: ASideBar Routes */}
-      {/* BEGIN :: HeaderBar Routes */}
-      <Routes>
-        {menuItems.map((item) => (
-          <Route path={item.path} element={<HeaderBar />} />
-        ))}
-        <Route path="settings" element={<HeaderBar />} />
-        <Route path="hr-members" element={<HeaderBar />} />
-        <Route path="profile" element={<HeaderBar />} />
-      </Routes>
-      {/* END :: HeaderBar Routes */}
     </BrowserRouter>
   );
 }

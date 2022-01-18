b79d9b2576deda4fb7e73edfcf55a05958a81484
Baturalp Sönmez <baturalpsonmez2002@gmail.com>
2022-01-18 19:06:38 +0300

    asidebar route'ları eklendi

--- crm-ui/src/App.tsx
+++ crm-ui/src/App.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import "./App.css";
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ASideBar from "./Layouts/ASideBar";
+import menuItems from "./asidemenu";
 
 function App() {
   return (
-    <>
-      <ASideBar />
-    </>
+    <BrowserRouter>
+      {/* BEGIN :: ASideBar Routes */}
+      <Routes>
+        {menuItems.map((item) => (
+          <Route path={item.path} element={<ASideBar />} />
+        ))}
+        <Route path="settings" element={<ASideBar />} />
+        <Route path="hr-members" element={<ASideBar />} />
+      </Routes>
+      {/* END :: ASideBar Routes */}
+    </BrowserRouter>
   );
 }
 

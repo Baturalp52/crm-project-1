7feaadbe1d81db04b3beefb13627e0637ed05318
Baturalp Sönmez <baturalpsonmez2002@gmail.com>
2022-01-20 15:31:01 +0300

    company mockData eklendi

---
+++ crm-ui/src/mockData/companies.ts
@@ -0,0 +1,25 @@
+import { ICompany } from "./interfaces/Company";
+
+const companies: ICompany[] = [
+  { id: 1, name: "Example Company", city: "Ankara" },
+  { id: 2, name: "Example Company", city: "Ankara" },
+  { id: 3, name: "Example Company", city: "Ankara" },
+  { id: 4, name: "Example Company", city: "Ankara" },
+  { id: 5, name: "Example Company", city: "Ankara" },
+  { id: 6, name: "Example Company", city: "Ankara" },
+  { id: 7, name: "Example Company", city: "Ankara" },
+  { id: 8, name: "Example Company", city: "Ankara" },
+  { id: 9, name: "Example Company", city: "Ankara" },
+  { id: 10, name: "Example Company", city: "Ankara" },
+  { id: 11, name: "Example Company", city: "Ankara" },
+  { id: 12, name: "Example Company", city: "Ankara" },
+  { id: 13, name: "Example Company", city: "Ankara" },
+  { id: 14, name: "Example Company", city: "Ankara" },
+  { id: 15, name: "Example Company", city: "Ankara" },
+  { id: 16, name: "Example Company", city: "Ankara" },
+  { id: 17, name: "Example Company", city: "Ankara" },
+  { id: 18, name: "Example Company", city: "Ankara" },
+  { id: 19, name: "Example Company", city: "Ankara" },
+];
+
+export default companies;

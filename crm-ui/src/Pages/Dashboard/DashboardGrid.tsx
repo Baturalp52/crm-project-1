cff44056b5ff247079a60f0343acd2d7daab163a
Baturalp Sönmez <baturalpsonmez2002@gmail.com>
2022-01-20 15:44:24 +0300

    Companies Dashboard'a eklendi

--- crm-ui/src/Pages/Dashboard/DashboardGrid.tsx
+++ crm-ui/src/Pages/Dashboard/DashboardGrid.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Grid } from "@mui/material";
 import GridItem from "./GridItem";
-import { People } from "@mui/icons-material";
+import { Apartment, People } from "@mui/icons-material";
 import candidates from "../../mockData/candidates";
+import companies from "../../mockData/companies";
 
 const DashboardGrid = () => {
   return (
@@ -17,6 +18,12 @@ const DashboardGrid = () => {
         icon={<People />}
         data={candidates.length}
         color="#9c27b0"
+      />
+      <GridItem
+        title="Companies"
+        icon={<Apartment />}
+        data={companies.length}
+        color="#0288d1"
       />
     </Grid>
   );

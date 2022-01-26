a2d235333e3bb5b05d96545d714c13b6be2337a3
Baturalp Sönmez <baturalpsonmez2002@gmail.com>
2022-01-26 18:52:51 +0300

    tasks crudtable eklendi

--- crm-ui/src/Pages/Tasks/TasksTable.tsx
+++ crm-ui/src/Pages/Tasks/TasksTable.tsx
@@ -1,30 +1,13 @@
 import React, { useState } from "react";
-import {
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TablePagination,
-  TableRow,
-  Checkbox,
-  ButtonGroup,
-  Button,
-  Stack,
-} from "@mui/material";
 
 import tasks from "../../mockData/tasks";
-import { Add, BorderColor, DeleteForeverRounded } from "@mui/icons-material";
 import TaskModal from "./TaskModal";
 import { ITask } from "../../mockData/interfaces/Task";
 import { getChipOfSituation } from "./helpers";
 import { useTranslation } from "react-i18next";
+import CRUDTable from "../../components/CRUDTable";
 
 const TasksTable = () => {
-  const [currentPage, setCurrentPage] = useState<number>(0);
-  const [rowPerPage, setRowPerPage] = useState<number>(10);
-  const [selectedTasksId, setSelectedTasksId] = useState<number[]>([]);
   const [isTaskModalOpen, setIsTaskModalOpen] = useState<boolean>(false);
   const [taskModalTask, setTaskModalTask] = useState<ITask | undefined>(
     undefined
@@ -39,124 +22,16 @@ const TasksTable = () => {
         isOpen={isTaskModalOpen}
         setIsOpen={setIsTaskModalOpen}
       />
-      <Paper
-        sx={{
-          width: "calc(100% - 40px)",
-          marginLeft: "auto",
-          overflow: "hidden",
-          boxShadow: "none",
+      <CRUDTable<ITask>
+        data={tasks}
+        cellNames={[t("id"), t("name"), t("description"), t("situation")]}
+        keysToShow={["id", "name", "description", "situation"]}
+        setModalData={setTaskModalTask}
+        setIsDataModalOpen={setIsTaskModalOpen}
+        customDataComponent={{
+          situation: (data: string) => getChipOfSituation(data),
         }}
-      >
-        <TableContainer sx={{ maxHeight: "500px" }}>
-          <Table stickyHeader aria-label="sticky table">
-            <TableHead>
-              <TableRow>
-                <TableCell>
-                  <Checkbox
-                    value="all"
-                    checked={tasks.length === selectedTasksId.length}
-                    onChange={(e) => {
-                      if (e.target.checked) {
-                        setSelectedTasksId(tasks.map((c) => c.id));
-                      } else setSelectedTasksId([]);
-                    }}
-                  />
-                </TableCell>
-                <TableCell>{t("id")}</TableCell>
-                <TableCell>{t("name")}</TableCell>
-                <TableCell>{t("description")}</TableCell>
-                <TableCell>{t("situation")}</TableCell>
-                <TableCell />
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {tasks
-                .slice(currentPage * rowPerPage, (currentPage + 1) * rowPerPage)
-                .map((item, index) => (
-                  <TableRow key={index}>
-                    <TableCell>
-                      <Checkbox
-                        value={item.id}
-                        checked={selectedTasksId.includes(item.id)}
-                        onChange={(e) => {
-                          if (e.target.checked) {
-                            const _selectedTasksId = [...selectedTasksId];
-                            _selectedTasksId.push(item.id);
-                            setSelectedTasksId(_selectedTasksId);
-                          } else {
-                            const _selectedTasksId = [...selectedTasksId];
-                            _selectedTasksId.splice(
-                              _selectedTasksId.indexOf(item.id),
-                              1
-                            );
-                            setSelectedTasksId(_selectedTasksId);
-                          }
-                        }}
-                      />
-                    </TableCell>
-                    <TableCell>{item.id}</TableCell>
-                    <TableCell>{item.name}</TableCell>
-                    <TableCell>{item.description}</TableCell>
-                    <TableCell>{getChipOfSituation(item.situation)}</TableCell>
-                    <TableCell>
-                      <Button
-                        sx={{ border: "none !important" }}
-                        color="warning"
-                        variant="contained"
-                        onClick={() => {
-                          setTaskModalTask(item);
-                          setIsTaskModalOpen(true);
-                        }}
-                      >
-                        <BorderColor />
-                      </Button>
-                    </TableCell>
-                  </TableRow>
-                ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <TablePagination
-          rowsPerPageOptions={[10, 25, 100]}
-          component="div"
-          count={tasks.length}
-          rowsPerPage={rowPerPage}
-          page={currentPage}
-          onPageChange={(e: unknown, newPage: number) => {
-            setCurrentPage(newPage);
-          }}
-          onRowsPerPageChange={(e) => {
-            setRowPerPage(+e.target.value);
-            setCurrentPage(0);
-          }}
-        />
-        <Stack padding={"1rem"}>
-          <ButtonGroup
-            sx={{ marginLeft: "auto" }}
-            aria-label="medium button group contained"
-            variant="contained"
-          >
-            <Button
-              sx={{ border: "none !important" }}
-              startIcon={<Add />}
-              color="success"
-              onClick={() => {
-                setTaskModalTask(undefined);
-                setIsTaskModalOpen(true);
-              }}
-            >
-              {t("add")}
-            </Button>
-            <Button
-              sx={{ border: "none !important" }}
-              startIcon={<DeleteForeverRounded />}
-              color="error"
-            >
-              {t("delete")}
-            </Button>
-          </ButtonGroup>
-        </Stack>
-      </Paper>
+      />
     </>
   );
 };

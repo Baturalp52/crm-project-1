92a03a9eb7f14914f3c5381dee63c9383c2c0cd6
Baturalp Sönmez <baturalpsonmez2002@gmail.com>
2022-01-26 18:06:09 +0300

    CRUDTable candidates'e eklendi

--- crm-ui/src/Pages/Candidates/CandidatesTable.tsx
+++ crm-ui/src/Pages/Candidates/CandidatesTable.tsx
@@ -1,33 +1,13 @@
 import React, { useState } from "react";
-import {
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TablePagination,
-  TableRow,
-  Checkbox,
-  ButtonGroup,
-  Button,
-  Stack,
-} from "@mui/material";
 
 import candidates from "../../mockData/candidates";
-import { Add, BorderColor, DeleteForeverRounded } from "@mui/icons-material";
 import CandidateModal from "./CandidateModal";
 import { ICandidate } from "../../mockData/interfaces/Candidate";
 import { useTranslation } from "react-i18next";
+import CRUDTable from "../../components/CRUDTable";
 
 const CandidatesTable = () => {
   const { t } = useTranslation("pages", { keyPrefix: "candidates.table" });
-
-  const [currentPage, setCurrentPage] = useState<number>(0);
-  const [rowPerPage, setRowPerPage] = useState<number>(10);
-  const [selectedCandidatesId, setSelectedCandidatesId] = useState<number[]>(
-    []
-  );
   const [isCandidateModalOpen, setIsCandidateModalOpen] =
     useState<boolean>(false);
   const [candidateModalCandidate, setCandidateModalCandidate] = useState<
@@ -41,128 +21,13 @@ const CandidatesTable = () => {
         isOpen={isCandidateModalOpen}
         setIsOpen={setIsCandidateModalOpen}
       />
-      <Paper
-        sx={{
-          width: "calc(100% - 40px)",
-          marginLeft: "auto",
-          overflow: "hidden",
-          boxShadow: "none",
-        }}
-      >
-        <TableContainer sx={{ maxHeight: "500px" }}>
-          <Table stickyHeader aria-label="sticky table">
-            <TableHead>
-              <TableRow>
-                <TableCell>
-                  <Checkbox
-                    value="all"
-                    checked={candidates.length === selectedCandidatesId.length}
-                    onChange={(e) => {
-                      if (e.target.checked) {
-                        setSelectedCandidatesId(candidates.map((c) => c.id));
-                      } else setSelectedCandidatesId([]);
-                    }}
-                  />
-                </TableCell>
-                <TableCell>{t("id")}</TableCell>
-                <TableCell>{t("name")}</TableCell>
-                <TableCell>{t("surname")}</TableCell>
-                <TableCell>{t("city")}</TableCell>
-                <TableCell />
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {candidates
-                .slice(currentPage * rowPerPage, (currentPage + 1) * rowPerPage)
-                .map((item, index) => (
-                  <TableRow key={index}>
-                    <TableCell>
-                      <Checkbox
-                        value={item.id}
-                        checked={selectedCandidatesId.includes(item.id)}
-                        onChange={(e) => {
-                          if (e.target.checked) {
-                            const _selectedCandidatesId = [
-                              ...selectedCandidatesId,
-                            ];
-                            _selectedCandidatesId.push(item.id);
-                            setSelectedCandidatesId(_selectedCandidatesId);
-                          } else {
-                            const _selectedCandidatesId = [
-                              ...selectedCandidatesId,
-                            ];
-                            _selectedCandidatesId.splice(
-                              _selectedCandidatesId.indexOf(item.id),
-                              1
-                            );
-                            setSelectedCandidatesId(_selectedCandidatesId);
-                          }
-                        }}
-                      />
-                    </TableCell>
-                    <TableCell>{item.id}</TableCell>
-                    <TableCell>{item.name}</TableCell>
-                    <TableCell>{item.surname}</TableCell>
-                    <TableCell>{item.city}</TableCell>
-                    <TableCell>
-                      <Button
-                        sx={{ border: "none !important" }}
-                        color="warning"
-                        variant="contained"
-                        onClick={() => {
-                          setCandidateModalCandidate(item);
-                          setIsCandidateModalOpen(true);
-                        }}
-                      >
-                        <BorderColor />
-                      </Button>
-                    </TableCell>
-                  </TableRow>
-                ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <TablePagination
-          rowsPerPageOptions={[10, 25, 100]}
-          component="div"
-          count={candidates.length}
-          rowsPerPage={rowPerPage}
-          page={currentPage}
-          onPageChange={(e: unknown, newPage: number) => {
-            setCurrentPage(newPage);
-          }}
-          onRowsPerPageChange={(e) => {
-            setRowPerPage(+e.target.value);
-            setCurrentPage(0);
-          }}
-        />
-        <Stack padding={"1rem"}>
-          <ButtonGroup
-            sx={{ marginLeft: "auto" }}
-            aria-label="medium button group contained"
-            variant="contained"
-          >
-            <Button
-              sx={{ border: "none !important" }}
-              startIcon={<Add />}
-              color="success"
-              onClick={() => {
-                setCandidateModalCandidate(undefined);
-                setIsCandidateModalOpen(true);
-              }}
-            >
-              {t("add")}
-            </Button>
-            <Button
-              sx={{ border: "none !important" }}
-              startIcon={<DeleteForeverRounded />}
-              color="error"
-            >
-              {t("delete")}
-            </Button>
-          </ButtonGroup>
-        </Stack>
-      </Paper>
+      <CRUDTable<ICandidate>
+        data={candidates}
+        cellNames={[t("id"), t("name"), t("surname"), t("city")]}
+        keysToShow={["id", "name", "surname", "city"]}
+        setModalData={setCandidateModalCandidate}
+        setIsDataModalOpen={setIsCandidateModalOpen}
+      />
     </>
   );
 };

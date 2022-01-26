a041d322d89317aaf61cca25b6eab72f0f163605
Baturalp Sönmez <baturalpsonmez2002@gmail.com>
2022-01-26 18:10:29 +0300

    CRUDTable companies'e eklendi

--- crm-ui/src/Pages/Companies/CompaniesTable.tsx
+++ crm-ui/src/Pages/Companies/CompaniesTable.tsx
@@ -1,31 +1,13 @@
 import React, { useState } from "react";
-import {
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TablePagination,
-  TableRow,
-  Checkbox,
-  ButtonGroup,
-  Button,
-  Stack,
-} from "@mui/material";
 
 import companies from "../../mockData/companies";
-import { Add, BorderColor, DeleteForeverRounded } from "@mui/icons-material";
 import CompanyModal from "./CompanyModal";
 import { ICompany } from "../../mockData/interfaces/Company";
 import { useTranslation } from "react-i18next";
+import CRUDTable from "../../components/CRUDTable";
 
 const CompanysTable = () => {
   const { t } = useTranslation("pages", { keyPrefix: "companies.table" });
-
-  const [currentPage, setCurrentPage] = useState<number>(0);
-  const [rowPerPage, setRowPerPage] = useState<number>(10);
-  const [selectedCompanysId, setSelectedCompanysId] = useState<number[]>([]);
   const [isCompanyModalOpen, setIsCompanyModalOpen] = useState<boolean>(false);
   const [companyModalCompany, setCompanyModalCompany] = useState<
     ICompany | undefined
@@ -38,122 +20,13 @@ const CompanysTable = () => {
         isOpen={isCompanyModalOpen}
         setIsOpen={setIsCompanyModalOpen}
       />
-      <Paper
-        sx={{
-          width: "calc(100% - 40px)",
-          marginLeft: "auto",
-          overflow: "hidden",
-          boxShadow: "none",
-        }}
-      >
-        <TableContainer sx={{ maxHeight: "500px" }}>
-          <Table stickyHeader aria-label="sticky table">
-            <TableHead>
-              <TableRow>
-                <TableCell>
-                  <Checkbox
-                    value="all"
-                    checked={companies.length === selectedCompanysId.length}
-                    onChange={(e) => {
-                      if (e.target.checked) {
-                        setSelectedCompanysId(companies.map((c) => c.id));
-                      } else setSelectedCompanysId([]);
-                    }}
-                  />
-                </TableCell>
-                <TableCell>{t("id")}</TableCell>
-                <TableCell>{t("name")}</TableCell>
-                <TableCell>{t("city")}</TableCell>
-                <TableCell />
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {companies
-                .slice(currentPage * rowPerPage, (currentPage + 1) * rowPerPage)
-                .map((item, index) => (
-                  <TableRow key={index}>
-                    <TableCell>
-                      <Checkbox
-                        value={item.id}
-                        checked={selectedCompanysId.includes(item.id)}
-                        onChange={(e) => {
-                          if (e.target.checked) {
-                            const _selectedCompanysId = [...selectedCompanysId];
-                            _selectedCompanysId.push(item.id);
-                            setSelectedCompanysId(_selectedCompanysId);
-                          } else {
-                            const _selectedCompanysId = [...selectedCompanysId];
-                            _selectedCompanysId.splice(
-                              _selectedCompanysId.indexOf(item.id),
-                              1
-                            );
-                            setSelectedCompanysId(_selectedCompanysId);
-                          }
-                        }}
-                      />
-                    </TableCell>
-                    <TableCell>{item.id}</TableCell>
-                    <TableCell>{item.name}</TableCell>
-                    <TableCell>{item.city}</TableCell>
-                    <TableCell>
-                      <Button
-                        sx={{ border: "none !important" }}
-                        color="warning"
-                        variant="contained"
-                        onClick={() => {
-                          setCompanyModalCompany(item);
-                          setIsCompanyModalOpen(true);
-                        }}
-                      >
-                        <BorderColor />
-                      </Button>
-                    </TableCell>
-                  </TableRow>
-                ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <TablePagination
-          rowsPerPageOptions={[10, 25, 100]}
-          component="div"
-          count={companies.length}
-          rowsPerPage={rowPerPage}
-          page={currentPage}
-          onPageChange={(e: unknown, newPage: number) => {
-            setCurrentPage(newPage);
-          }}
-          onRowsPerPageChange={(e) => {
-            setRowPerPage(+e.target.value);
-            setCurrentPage(0);
-          }}
-        />
-        <Stack padding={"1rem"}>
-          <ButtonGroup
-            sx={{ marginLeft: "auto" }}
-            aria-label="medium button group contained"
-            variant="contained"
-          >
-            <Button
-              sx={{ border: "none !important" }}
-              startIcon={<Add />}
-              color="success"
-              onClick={() => {
-                setCompanyModalCompany(undefined);
-                setIsCompanyModalOpen(true);
-              }}
-            >
-              {t("add")}
-            </Button>
-            <Button
-              sx={{ border: "none !important" }}
-              startIcon={<DeleteForeverRounded />}
-              color="error"
-            >
-              {t("delete")}
-            </Button>
-          </ButtonGroup>
-        </Stack>
-      </Paper>
+      <CRUDTable<ICompany>
+        data={companies}
+        cellNames={[t("id"), t("name"), t("city")]}
+        keysToShow={["id", "name", "city"]}
+        setModalData={setCompanyModalCompany}
+        setIsDataModalOpen={setIsCompanyModalOpen}
+      />
     </>
   );
 };

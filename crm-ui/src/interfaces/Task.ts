97c3c74634dd109d4e4dd62f8d0c5d5d1514d9ba
Baturalp Sönmez <baturalpsonmez2002@gmail.com>
2022-01-27 16:41:28 +0300

    task interface'inde güncelleme

--- crm-ui/src/interfaces/Task.ts
+++ crm-ui/src/interfaces/Task.ts
@@ -1,9 +1,14 @@
+import { ICandidate } from "./Candidate";
 import { IHRMember } from "./HRMember";
 
 export interface ITask {
   id: number;
   name: string;
   description?: string;
-  assignedMember?: IHRMember;
+  assignedMember?: IHRMember | "global";
+  assignedCandidate?: ICandidate;
+  comments?: string[];
   situation: "on-progress" | "completed" | "closed" | "open";
+  createdDate?: Date;
+  endDate?: Date | undefined;
 }

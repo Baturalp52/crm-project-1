aabf8da210ec7130f5827e4f96e0e96bb590561f
Baturalp Sönmez <baturalpsonmez2002@gmail.com>
2022-01-27 19:18:36 +0300

    Updates

---
+++ static/js/619.598c2ed2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcrm_ui=self.webpackChunkcrm_ui||[]).push([[619],{4619:function(u,e,n){n.r(e);n(2791);var r=n(184);e.default=function(){return(0,r.jsx)(r.Fragment,{})}}}]);
+//# sourceMappingURL=619.598c2ed2.chunk.js.map

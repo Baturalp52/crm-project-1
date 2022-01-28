bf7b5ba743eb9cec5353723b1ffed4a62a16c1a0
Baturalp Sönmez <baturalpsonmez2002@gmail.com>
2022-01-28 14:38:01 +0300

    formdropdown eklendi

---
+++ crm-ui/src/components/FormDropdown/index.tsx
@@ -0,0 +1,36 @@
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import React from "react";
+
+interface IFormDropdownProps<DataType> {
+  label: string;
+  handleChange: (e: any) => void;
+  datas: DataType[];
+  defaultValue: string;
+  dataToValue: (data: DataType) => string;
+  selectedId: number;
+}
+
+const FormDropdown = <DataType extends { id: number }>(
+  props: IFormDropdownProps<DataType>
+) => {
+  const { label, handleChange, defaultValue, datas, dataToValue, selectedId } =
+    props;
+  return (
+    <FormControl sx={{ m: 1, width: "100%" }}>
+      <InputLabel>{label}</InputLabel>
+
+      <Select value={selectedId} label={label} onChange={handleChange}>
+        <MenuItem value={0}>
+          <em>{defaultValue}</em>
+        </MenuItem>
+        {datas.map((data: DataType) => (
+          <MenuItem key={data.id} value={data.id}>
+            {dataToValue(data)}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
+
+export default FormDropdown;

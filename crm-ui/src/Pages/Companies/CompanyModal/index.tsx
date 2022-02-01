48d67ab1e4d8f9ccf96046d5d4a8d5a76398ec34
Baturalp Sönmez <baturalpsonmez2002@gmail.com>
2022-02-01 09:44:35 +0300

    companyModal düzenlemesi

--- crm-ui/src/Pages/Companies/CompanyModal/index.tsx
+++ crm-ui/src/Pages/Companies/CompanyModal/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { List, ListItem } from "@mui/material";
 import { useFormik } from "formik";
-import { emptyCompany } from "../emptyCompany";
-import FormInput from "../../../components/FormInput";
-import { ICompany } from "../../../interfaces/Company";
-import ActionModal from "../../../components/ActionModal";
 import { useTranslation } from "react-i18next";
+
+import { Grid, List, ListItem } from "@mui/material";
+import FormInput from "../../../components/FormInput";
+import ActionModal from "../../../components/ActionModal";
+
+import { ICompany } from "../../../interfaces/Company";
+import { emptyCompany } from "../emptyCompany";
 
 interface ICompanyModalProps {
   company?: ICompany;
@@ -29,54 +31,58 @@ const CompanyModal = (props: ICompanyModalProps) => {
       title={form.values.id ? t("edit") : t("add")}
       saveFunction={() => {}}
     >
-      <List>
-        <ListItem>
-          <FormInput
-            label={t("form.id")}
-            type="number"
-            value={form.values.id}
-            name="id"
-            onChange={form.handleChange}
-            disabled
-          />
-        </ListItem>
-        <ListItem>
-          <FormInput
-            label={t("form.name")}
-            type="text"
-            value={form.values.name}
-            name="name"
-            onChange={form.handleChange}
-          />
-        </ListItem>
-        <ListItem>
-          <FormInput
-            label={t("form.address")}
-            type="text"
-            value={form.values.address}
-            name="address"
-            onChange={form.handleChange}
-          />
-        </ListItem>
-        <ListItem>
-          <FormInput
-            label={t("form.city")}
-            type="text"
-            value={form.values.city}
-            name="city"
-            onChange={form.handleChange}
-          />
-        </ListItem>
-        <ListItem>
-          <FormInput
-            label={t("form.sector")}
-            type="text"
-            value={form.values.sector}
-            name="sector"
-            onChange={form.handleChange}
-          />
-        </ListItem>
-      </List>
+      <Grid container padding={2} spacing={2}>
+        <Grid xs={12} md={6} item>
+          <List>
+            <ListItem>
+              <FormInput
+                label={t("form.id")}
+                type="number"
+                value={form.values.id}
+                name="id"
+                onChange={form.handleChange}
+                disabled
+              />
+            </ListItem>
+            <ListItem>
+              <FormInput
+                label={t("form.name")}
+                type="text"
+                value={form.values.name}
+                name="name"
+                onChange={form.handleChange}
+              />
+            </ListItem>
+            <ListItem>
+              <FormInput
+                label={t("form.address")}
+                type="text"
+                value={form.values.address}
+                name="address"
+                onChange={form.handleChange}
+              />
+            </ListItem>
+            <ListItem>
+              <FormInput
+                label={t("form.city")}
+                type="text"
+                value={form.values.city}
+                name="city"
+                onChange={form.handleChange}
+              />
+            </ListItem>
+            <ListItem>
+              <FormInput
+                label={t("form.sector")}
+                type="text"
+                value={form.values.sector}
+                name="sector"
+                onChange={form.handleChange}
+              />
+            </ListItem>
+          </List>
+        </Grid>
+      </Grid>
     </ActionModal>
   );
 };

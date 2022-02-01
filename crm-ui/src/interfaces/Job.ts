399a107eb5250a2b317d9f7aa1f49047a18a6524
Baturalp Sönmez <baturalpsonmez2002@gmail.com>
2022-02-01 09:45:18 +0300

    job'a company eklendi

--- crm-ui/src/interfaces/Job.ts
+++ crm-ui/src/interfaces/Job.ts
@@ -1,7 +1,10 @@
+import { ICompany } from "./Company";
+
 export interface IJob {
   id: number;
   name: string;
   experience?: number;
   salaryExpectation?: number;
   studyFields?: string[];
+  company?: ICompany;
 }

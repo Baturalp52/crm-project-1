6e3b57e62503aecbc4013fd7e38c6e9196511c2c
Baturalp Sönmez <baturalpsonmez2002@gmail.com>
2022-02-18 09:23:46 +0300

    genel candidate işlemleri yapıldı

--- crm-api/api/v1/candidates/views.py
+++ crm-api/api/v1/candidates/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+from json import loads
+from django.http import HttpResponse,JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-def index():
-    return 1
+from api.v1.candidates.models import Candidate
+
+@csrf_exempt
+def index(request):
+    if request.method == "GET":
+        return JsonResponse(list(Candidate.objects.all().values()),safe=False)
+    elif request.method == "POST":
+        newCandidate = Candidate()
+        newCandidate.mapsCoord = {}
+        for key,value in loads(request.body).items():
+            if type(value) is dict:
+                pass
+            else:
+                setattr(newCandidate,key,value)
+        
+        newCandidate.save()
+        return HttpResponse(Candidate.objects.all())
+    else:
+        return HttpResponse()

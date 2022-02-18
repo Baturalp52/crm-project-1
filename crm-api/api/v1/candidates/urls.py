5889b2a7f430905dfce2895db742b7beb477d1a8
Baturalp Sönmez <baturalpsonmez2002@gmail.com>
2022-02-18 10:15:48 +0300

    update eklendi

--- crm-api/api/v1/candidates/urls.py
+++ crm-api/api/v1/candidates/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
-from api.v1.candidates.views import index
+from api.v1.candidates.views import get_or_create
+from api.v1.candidates.views import update
 
 
-urlpatterns = [path("",index)]
+urlpatterns = [path("",get_or_create),
+path("<int:id>",update)]
--- crm-api/api/v1/candidates/views.py
+++ crm-api/api/v1/candidates/views.py
@@ -1,11 +1,12 @@
-from json import loads
-from django.http import HttpResponse,JsonResponse
+from json import loads,dumps
+from django.forms import model_to_dict
+from django.http import Http404, HttpResponse,JsonResponse,HttpResponseNotAllowed
 from django.views.decorators.csrf import csrf_exempt
 
 from api.v1.candidates.models import Candidate
 
 @csrf_exempt
-def index(request):
+def get_or_create(request):
     if request.method == "GET":
         return JsonResponse(list(Candidate.objects.all().values()),safe=False)
     elif request.method == "POST":
@@ -18,6 +19,26 @@ def index(request):
                 setattr(newCandidate,key,value)
         
         newCandidate.save()
-        return HttpResponse(Candidate.objects.all())
+        return JsonResponse(Candidate.objects.all(),safe=False)
     else:
-        return HttpResponse()
+        return HttpResponseNotAllowed()
+
+
+@csrf_exempt
+def update(request,id):
+    if request.method == "PUT":
+        candidates = Candidate.objects.filter(id=id)
+        if(len(candidates)>0):
+            candidate = candidates[0]
+            for key,value in loads(request.body).items():
+                if type(value) is dict:
+                    pass
+                else:
+                    setattr(candidate,key,value)
+            candidate.save()
+
+            return JsonResponse(model_to_dict(candidate),safe=False)
+        else:
+            return Http404()
+    else:
+        return HttpResponseNotAllowed()
